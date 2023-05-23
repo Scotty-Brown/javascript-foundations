4c570c5f2cf9ce36eb23c37f8050d9c674452b85
Scotty Brown <scottybrown98@gmail.com>
2023-05-22 21:47:42 -0700

    complete tacoStand test suite

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -1,2 +1,34 @@
 
-module.exports = {}
+function createIngredient(name, price) {
+    return {
+        name: name ?? 'unknown',
+        price: price ?? 0.00
+    }
+}
+
+function createTaco(name, ingredList) {
+    return {
+        name: name ?? 'custom',
+        ingredients: ingredList ?? []
+    }
+}
+
+function addIngredientToTaco(taco, ingredient) {
+    taco.ingredients.push(ingredient)
+        return taco
+}
+
+function calculatePrice(taco) {
+    var sum = 0
+    for (var i = 0; i < taco.ingredients.length; i++) {
+    sum += taco.ingredients[i].price
+  } return sum
+}
+
+
+module.exports = {
+    createIngredient,
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice
+}
